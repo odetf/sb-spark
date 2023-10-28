3f94f628a320d2969c87fb1e1f8b30ad17576ffa
odetf <olenkatarasenko79@gmail.com>
2023-10-28 15:07:06 +0300

    Добавила main метод в класс

--- lab04a/filter/build.sbt
+++ lab04a/filter/build.sbt
@@ -4,7 +4,7 @@ ThisBuild / scalaVersion := "2.11.12"
 
 lazy val root = (project in file("."))
   .settings(
-    name := "lab4a"
+    name := "filter"
   )
 
 libraryDependencies += "org.apache.spark" % "spark-core_2.11" % "2.4.7"
--- lab04a/filter/src/main/scala/filter.scala
+++ lab04a/filter/src/main/scala/filter.scala
@@ -1,47 +1,49 @@
 class filter {
 
-  import org.apache.spark.sql.SparkSession
-  import org.apache.spark.sql.types._
-  import org.apache.spark.sql._
-  import org.apache.spark.sql.functions._
+  def main(args: Array[String]): Unit = {
 
-  val spark = SparkSession.builder().appName("lab04").getOrCreate()
+    import org.apache.spark.sql.SparkSession
+    import org.apache.spark.sql.types._
+    import org.apache.spark.sql._
+    import org.apache.spark.sql.functions._
 
-  val topic_name: String = spark.sparkContext.getConf.get("spark.filter.topic_name")
-  val offset: String = spark.sparkContext.getConf.get("spark.filter.offset")
-  val output_dir_prefix: String = spark.sparkContext.getConf.get("spark.filter.output_dir_prefix")
+    val spark = SparkSession.builder().appName("lab04").getOrCreate()
 
-  spark.conf.set("spark.sql.session.timeZone", "UTC")
+    val topic_name: String = spark.sparkContext.getConf.get("spark.filter.topic_name")
+    val offset: String = spark.sparkContext.getConf.get("spark.filter.offset")
+    val output_dir_prefix: String = spark.sparkContext.getConf.get("spark.filter.output_dir_prefix")
 
-  import spark.implicits._
+    spark.conf.set("spark.sql.session.timeZone", "UTC")
 
-
-  val kafkaParams = Map("kafka.bootstrap.servers" -> "spark-master-1:6667",
-    "subscribe" -> topic_name,
-    "startingOffsets" -> (if (offset.contains("earliest"))
-      offset
-    else {
-      "{\"" + topic_name + "\":{\"0\":" + offset + "}}"
-    }))
-  val kafkaDF = spark.read.format("kafka").options(kafkaParams).load
-
-  val jsonTransformedDF = spark.read.json(kafkaDF.select('value.cast("string")).as[String])
-    .withColumn("date",
-      date_format(from_unixtime($"timestamp" / 1000).cast("timestamp"), "yyyyMMdd").cast("string"))
-
-  jsonTransformedDF.withColumn("p_date", $"date")
-    .filter($"event_type" === "buy")
-    .write
-    .partitionBy("p_date")
-    .json(output_dir_prefix + "/buy/")
-
-  jsonTransformedDF.withColumn("p_date", $"date")
-    .filter($"event_type" === "view")
-    .write
-    .partitionBy("p_date")
-    .json(output_dir_prefix + "/view/")
-
-  spark.stop()
+    import spark.implicits._
 
 
+    val kafkaParams = Map("kafka.bootstrap.servers" -> "spark-master-1:6667",
+      "subscribe" -> topic_name,
+      "startingOffsets" -> (if (offset.contains("earliest"))
+        offset
+      else {
+        "{\"" + topic_name + "\":{\"0\":" + offset + "}}"
+      }))
+    val kafkaDF = spark.read.format("kafka").options(kafkaParams).load
+
+    val jsonTransformedDF = spark.read.json(kafkaDF.select('value.cast("string")).as[String])
+      .withColumn("date",
+        date_format(from_unixtime($"timestamp" / 1000).cast("timestamp"), "yyyyMMdd").cast("string"))
+
+    jsonTransformedDF.withColumn("p_date", $"date")
+      .filter($"event_type" === "buy")
+      .write
+      .partitionBy("p_date")
+      .json(output_dir_prefix + "/buy/")
+
+    jsonTransformedDF.withColumn("p_date", $"date")
+      .filter($"event_type" === "view")
+      .write
+      .partitionBy("p_date")
+      .json(output_dir_prefix + "/view/")
+
+    spark.stop()
+
+  }
 }
